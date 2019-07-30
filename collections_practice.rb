807c7a82409f53d50a6d85b25916be6b62070bbf
Steven Hunal <stevehunal@gmail.com>
2019-07-30 01:29:43 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -27,15 +27,7 @@ def sort_array_char_count(array)
 end
 
 def swap_elements(array)
-  array.sort do |a, b|
-  if a == b
-    0
-  elsif a < b
-    -1
-  elsif a > b
-    1
-  end
-  end
+  array
 end
 
 def reverse_array(array)
